33ace1e9df4ea2be997f1dae213618679b570f66
Elad Sopher <eladsopher@gmail.com>
2025-02-23 22:21:39 +0200

    changing stacks

--- scripts/s3_create.py
+++ scripts/s3_create.py
@@ -68,7 +68,7 @@ def create_bucket(access_type: str):
         pulumi.export("bucket_name", bucket.id)
 
     # Create or select the Pulumi stack
-    stack_name = "S3_Dev"
+    stack_name = "S3Dev"
     project_name = "AWS-Resource-Management"
 
     stack = auto.create_or_select_stack(
